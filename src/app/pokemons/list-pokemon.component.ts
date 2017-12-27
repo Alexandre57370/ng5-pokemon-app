e2cd6fdc3da59264b8f4f6ad3b9958f5914375c2
Simon Dieny <simon.dieny@gmail.com>
2017-12-27 23:39:32 +0100

    8.services

--- src/app/pokemons/list-pokemon.component.ts
+++ src/app/pokemons/list-pokemon.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Pokemon } from './pokemon';
 import { POKEMONS } from './mock-pokemons';
 import { Router } from '@angular/router';
+import { PokemonsService } from './pokemons.service';
 
 @Component({
 	selector: 'list-pokemon',
@@ -11,10 +12,14 @@ export class ListPokemonComponent implements OnInit {
 
 	pokemons: Pokemon[] = null;
 
-	constructor(private router: Router) { }
+	constructor(private router: Router, private pokemonsService: PokemonsService) { }
 
 	ngOnInit(): void {
-		this.pokemons = POKEMONS;
+		this.getPokemons();
+	}
+
+	getPokemons(): void {
+		this.pokemons = this.pokemonsService.getPokemons();
 	}
 
 	selectPokemon(pokemon: Pokemon): void {
