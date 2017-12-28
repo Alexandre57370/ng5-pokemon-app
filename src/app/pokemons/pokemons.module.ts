160b7b493d968917c47d8ed3062a24b10a54088c
Simon Dieny <simon.dieny@gmail.com>
2017-12-28 18:36:25 +0100

    10.http

--- src/app/pokemons/pokemons.module.ts
+++ src/app/pokemons/pokemons.module.ts
@@ -8,6 +8,7 @@ import { DetailPokemonComponent } from './detail-pokemon.component';
 import { EditPokemonComponent } from './edit-pokemon.component';
 import { PokemonFormComponent } from './pokemon-form.component';
 import { PokemonSearchComponent } from './search-pokemon.component';
+import { LoaderComponent } from '../loader.component';
 import { BorderCardDirective } from './border-card.directive';
 import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
 import { PokemonsService } from './pokemons.service';
@@ -24,6 +25,7 @@ import { PokemonsService } from './pokemons.service';
 		EditPokemonComponent,
 		PokemonFormComponent,
 		PokemonSearchComponent,
+		LoaderComponent,
 		BorderCardDirective,
 		PokemonTypeColorPipe
 	],
