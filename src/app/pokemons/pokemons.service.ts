0b5c41ff78d11fb403463eb1ad0e1c81736fc08c
Simon Dieny <simon.dieny@gmail.com>
2017-12-28 17:03:08 +0100

    feat(http) : add GET request

--- src/app/pokemons/pokemons.service.ts
+++ src/app/pokemons/pokemons.service.ts
@@ -1,28 +1,53 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Pokemon } from './pokemon';
 import { POKEMONS } from './mock-pokemons';
+import { Observable } from 'rxjs/Observable';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class PokemonsService {
 
-	// Retourne tous les pokémons
-	getPokemons(): Pokemon[] {
-		return POKEMONS;
+	private pokemonsUrl = 'api/pokemons';
+
+	constructor(private http: HttpClient) { }
+
+	/** GET pokemons */
+	getPokemons(): Observable<Pokemon[]> {
+		return this.http.get<Pokemon[]>(this.pokemonsUrl).pipe(
+			tap(_ => this.log(`fetched pokemons`)),
+			catchError(this.handleError('getPokemons', []))
+		);
 	}
 
-	// Retourne le pokémon avec l'identifiant passé en paramètre
-	getPokemon(id: number): Pokemon {
-		let pokemons = this.getPokemons();
+	/** GET pokemon */
+	getPokemon(id: number): Observable<Pokemon> {
+		const url = `${this.pokemonsUrl}/${id}`;
 
-		for (let index = 0; index < pokemons.length; index++) {
-			if (id === pokemons[index].id) {
-				return pokemons[index];
-			}
-		}
+		return this.http.get<Pokemon>(url).pipe(
+			tap(_ => this.log(`fetched pokemon id=${id}`)),
+			catchError(this.handleError<Pokemon>(`getPokemon id=${id}`))
+		);
 	}
 
-	// types de pokémons possible 
+	/** GET types */
 	getPokemonTypes(): Array<string> {
-		return ['Plante', 'Feu', 'Eau', 'Insecte', 'Normal', 'Electrik', 'Poison', 'Fée', 'Vol', 'Combat', 'Psy'];
+		return ['Plante', 'Feu', 'Eau', 'Insecte', 'Normal',
+			'Electrik', 'Poison', 'Fée', 'Vol', 'Combat', 'Psy'];
+	}
+
+	/* handleError */
+	private handleError<T>(operation = 'operation', result?: T) {
+		return (error: any): Observable<T> => {
+			console.error(error);
+			console.log(`${operation} failed: ${error.message}`);
+
+			return of(result as T);
+		};
+	}
+
+	/* log */
+	private log(log: string) {
+		console.info(log);
 	}
 }
